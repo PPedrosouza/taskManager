e92ae6164b217b18d4dd7ce67c2cec1dca3308c5
Pedro Souza <pedro.olegario23@gmail.com>
2025-03-17 21:44:50 -0300

    feat: add dictionary methods

--- Modulo_1/tipos-variaveis/dicionarios.py
+++ Modulo_1/tipos-variaveis/dicionarios.py
@@ -10,3 +10,30 @@ pessoa['sobrenome'] = "Silva"
 print("Sobrenome:", pessoa['sobrenome'])
 print("Idade:", pessoa['idade'])
 print("Cidade:", pessoa['cidade'])
+
+pessoa['sobrenome'] = 'Silva'
+print("Sobrenome:", pessoa['sobrenome'])
+
+pessoa['idade'] = 31
+print("Idade atualizada:", pessoa['idade'])
+
+# removendo um par chave-valor
+del pessoa['sobrenome']
+print(pessoa)
+
+# métodos: keys(), values(), items()
+chaves = pessoa.keys()
+# print("Chaves do dicioário:", chaves)
+# print("Primeira chave:", chaves[0])
+
+chaves = list(pessoa.keys())
+print("Chaves do dicioário:", chaves)
+print("Primeira chave do dicionário:", chaves[0])
+
+valores = list(pessoa.values())
+print("Valores do dicioário:", valores)
+print("Primeira valor do dicionário:", valores[0])
+
+items = list(pessoa.items())
+print("Pares chave-valor do dicionário:", items)
+print("Primeira chave-valor: %s = %s" % (items[0][0], items[0][1]))
--- Modulo_1/tipos-variaveis/tupla.py
+++ Modulo_1/tipos-variaveis/tupla.py
@@ -1,7 +1,7 @@
 # imutáveis
 
 # Criando uma tupla de exemplo
-minha_tupla = (1, 2, 2, 3, 4, 3)
+minha_tupla = (1, 2, 2, 3, 4, 3) 
 print("Minha tupla:", minha_tupla)
 
 print("minha_tupla[0]:", minha_tupla[0])
@@ -11,8 +11,25 @@ print("minha_tupla[-1]:", minha_tupla[-1])
 
 # count
 contagem = minha_tupla.count(2)
-print("Quantidade de vezes que o elemento 2 aparece:", contagem)
+print("Quantidade de vezes que o elemento 2 aparece:", contagem, "vezes")
 
 # index
 indice = minha_tupla.index(3)
 print("Índice da primeira ocorrência do elemento 3:", indice)
+
+# exercícios
+
+# concatenação de tuplas       
+tupla1 = (1, 2, 3)
+tupla2 = (4, 5, 6)
+tupla_concatenada = tupla1 + tupla2
+print("Tupla concatenada:", tupla_concatenada)
+
+
+# desempacotamento de tuplas
+tupla = ("Python", "é", "incrível")
+a, b, c = tupla
+print(a)
+print(b)
+print(c)
+
