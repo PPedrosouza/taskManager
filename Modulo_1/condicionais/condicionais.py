0dfc52b3061c8cc15d035344c7311cdcd2391197
Pedro Souza <pedro.olegario23@gmail.com>
2025-03-25 21:01:04 -0300

    feat: conditionals if, elif, else

--- Modulo_1/condicionais/condicionais.py
+++ Modulo_1/condicionais/condicionais.py
@@ -39,6 +39,6 @@ else:
     print("Você é menor de idade")
     
 
-# Exemplo de operador ternário
+# Exemplo de condição em uma única linha
 mensagem = "Pode tirar a carteira de habilitação" if idade >=18 else "Você não pode tirar a carteira de habilitação"
 print(mensagem)
